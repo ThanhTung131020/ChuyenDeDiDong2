2d1685a4decb6d7543f4c79788186c17e8d910ee
tranphan2503 <tranduchungphan@gmail.com>
2022-11-03 09:20:00 +0700

    phan commit

--- app/src/main/java/com/example/chuyendedidong2/Fragment/ThontincuahangFragment.java
+++ app/src/main/java/com/example/chuyendedidong2/Fragment/ThontincuahangFragment.java
@@ -9,33 +9,58 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.chuyendedidong2.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ThontincuahangFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class ThontincuahangFragment extends Fragment {
 
-    FirebaseDatabase database;
-    FirebaseAuth auth;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public ThontincuahangFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ThontincuahangFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ThontincuahangFragment newInstance(String param1, String param2) {
+        ThontincuahangFragment fragment = new ThontincuahangFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_thontincuahang, container, false);
-        database = FirebaseDatabase.getInstance();
-        auth = FirebaseAuth.getInstance();
-
-        return view;
+        return inflater.inflate(R.layout.fragment_thontincuahang, container, false);
     }
 }
