67d25875232ab4ea72616b6ca374b21636c2ddb6
TranDangKhoaID <trankhoa26102002@gmail.com>
2022-11-07 22:12:22 +0700

    update database dang ky va dang nhap

--- app/src/main/java/com/example/chuyendedidong2/RegistrationActivity.java
+++ app/src/main/java/com/example/chuyendedidong2/RegistrationActivity.java
@@ -7,21 +7,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.chuyendedidong2.Model.Personal;
+import com.example.chuyendedidong2.Model.Shipper;
+import com.example.chuyendedidong2.Model.Shop;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText email, password, cpassword, sdt, diachi, hoten;
     Button btnDangKy;
     RadioButton rdbCaNhan, rdbCuaHang, rdbShipper;
-    //private FirebaseAuth auth;
+    RadioGroup rdo;
+    Personal personal;
+    Shop shop;
+    Shipper shipper;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
 
 
     @Override
@@ -30,7 +43,8 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         //getSupportActionBar().hide();
-        //auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         //anh xa view
         setControl();
@@ -50,6 +64,7 @@ public class RegistrationActivity extends AppCompatActivity {
         rdbCaNhan = findViewById(R.id.rdbCaNhan);
         rdbCuaHang = findViewById(R.id.rdbCuaHang);
         rdbShipper = findViewById(R.id.rdbShipper);
+        rdo = findViewById(R.id.radioGroup);
     }
 
     private void setEvent() {
@@ -60,7 +75,6 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
-
     public void signUp(View view){
 
         String email = this.email.getText().toString();
@@ -95,18 +109,65 @@ public class RegistrationActivity extends AppCompatActivity {
             Toast.makeText(this,"Nhập địa chỉ!",Toast.LENGTH_SHORT).show();
             return;
         }
-//        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//                if(task.isSuccessful()){
-//                    Toast.makeText(RegistrationActivity.this,"Đăng ký thành công",Toast.LENGTH_SHORT).show();
-//                    startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
-//                }else {
-//                    Toast.makeText(RegistrationActivity.this,"Đăng ký thất bại"+task.getException(),Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
 
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    if (rdbCaNhan.isChecked()){
+                        personal = new Personal(auth.getUid(),hoten,sdt,diachi,email);
+                        addPersonalOnDataBase(personal);
+                    }else if(rdbCuaHang.isChecked()){
+                        shop = new Shop(auth.getUid(),hoten,sdt,diachi,email);
+                        addShopOnDataBase(shop);
+                    }else if(rdbShipper.isChecked()){
+                        shipper = new Shipper(auth.getUid(),hoten,sdt,diachi,email);
+                        addShipperOnDataBase(shipper);
+                    }
+                }else {
+                    Toast.makeText(RegistrationActivity.this,"Đăng ký thất bại"+task.getException(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void addShipperOnDataBase(Shipper shipper) {
+        DatabaseReference root = database.getReference("shipper");
+        String pathID = auth.getUid();
+        root.child(pathID).setValue(shipper, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(RegistrationActivity.this,"Đăng ký thành công",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void addShopOnDataBase(Shop shop) {
+        DatabaseReference root = database.getReference("shop");
+        String pathID = auth.getUid();
+        root.child(pathID).setValue(shop, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(RegistrationActivity.this,"Đăng ký thành công",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+    }
+    public  void addPersonalOnDataBase(Personal personal){
+        DatabaseReference root = database.getReference("personal");
+        String pathID = auth.getUid();
+        root.child(pathID).setValue(personal, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(RegistrationActivity.this,"Đăng ký thành công",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
     }
     public void signIn(View view){
         startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
